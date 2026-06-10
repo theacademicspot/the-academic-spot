49acf15d41d9687341e61b731bf22ddaa63997d5
Sumit Jha <0803eesjha@gmail.com>
2026-06-10 16:17:33 +0530

    disable print and right click

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,70 @@
+import { useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return <AppRoutes />;
+
+useEffect(() => {
+
+```
+// Disable Ctrl + P
+
+const handleKeyDown = (e) => {
+
+  if (
+    e.ctrlKey &&
+    e.key.toLowerCase() === "p"
+  ) {
+    e.preventDefault();
+    alert("Printing is disabled");
+  }
+
+  // Disable Ctrl + S
+
+  if (
+    e.ctrlKey &&
+    e.key.toLowerCase() === "s"
+  ) {
+    e.preventDefault();
+    alert("Saving is disabled");
+  }
+
+};
+
+// Disable Right Click
+
+const handleContextMenu = (e) => {
+  e.preventDefault();
+};
+
+window.addEventListener(
+  "keydown",
+  handleKeyDown
+);
+
+document.addEventListener(
+  "contextmenu",
+  handleContextMenu
+);
+
+return () => {
+
+  window.removeEventListener(
+    "keydown",
+    handleKeyDown
+  );
+
+  document.removeEventListener(
+    "contextmenu",
+    handleContextMenu
+  );
+
+};
+```
+
+}, []);
+
+return <AppRoutes />;
+
 }
 
 export default App;
