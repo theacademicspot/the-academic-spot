3afdb15abaae0ba7d02e5886b5a6bc34ab28a5d2
Sumit Jha <0803eesjha@gmail.com>
2026-06-12 15:22:48 +0530

    Fixed SMTP config

--- server/server.js
+++ server/server.js
@@ -79,14 +79,17 @@ app.post("/send-otp", async (req, res) => {
       [email, otp]
     );
 
-    const transporter =
-      nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
-      });
+   const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+await transporter.verify();
+console.log("SMTP Connected");
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
