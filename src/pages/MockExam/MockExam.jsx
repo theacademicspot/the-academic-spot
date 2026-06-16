38efbc21964feea575e5a23147ddf02318515b4b
Sumit Jha <0803eesjha@gmail.com>
2026-06-16 19:48:13 +0530

    fix mock result export

--- src/pages/MockExam/MockExam.jsx
+++ src/pages/MockExam/MockExam.jsx
@@ -1,17 +1,49 @@
 import "./MockExam.css";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import questions from "./data/questions";
 
 function MockExam() {
 
   const { id } = useParams();
 
+  const [currentQuestion, setCurrentQuestion] =
+    useState(0);
+
+  const question =
+    questions[currentQuestion];
+
   return (
 
-    <div>
+    <div className="mock-exam">
 
       <h1>
         Mock Test {id}
       </h1>
+
+      <h2>
+        Question {currentQuestion + 1}
+      </h2>
+
+      <p>
+        {question.question}
+      </p>
+
+      <div>
+
+        {question.options.map(
+          (option, index) => (
+
+            <button
+              key={index}
+            >
+              {option}
+            </button>
+
+          )
+        )}
+
+      </div>
 
     </div>
 
