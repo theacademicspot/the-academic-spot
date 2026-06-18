84ff8eaa3e71a08b59566277e18f7de62487441c
Sumit Jha <0803eesjha@gmail.com>
2026-06-18 18:21:59 +0530

    admin upload question

--- src/pages/MockExam/data/questions.js
+++ src/pages/MockExam/data/questions.js
@@ -2,89 +2,35 @@ const questions = [
 
   {
     id: 1,
-
-    question:
-      "Specific resistance depends on?",
-
-    options: [
-      "Length",
-      "Area",
-      "Material",
-      "Dimension"
-    ],
-
-    answer: 2
-  },
-
-  {
-    id: 2,
-
-    question:
-      "SI Unit of Force?",
-
-    options: [
-      "Newton",
-      "Joule",
-      "Pascal",
-      "Watt"
-    ],
-
-    answer: 0
-  }
-
-];
-
-export default questions;const questions = [
-
-  {
-    id: 1,
-
     subject: "Physics",
-
     chapter: "Current Electricity",
-
     difficulty: "Easy",
-
     marks: 2,
-
     negativeMarks: 1,
-
-    question:
-      "Specific resistance depends on?",
-
+    question: "Specific resistance depends on?",
     options: [
       "Length",
       "Area",
       "Material",
       "Dimension"
     ],
-
     answer: 2
   },
 
   {
     id: 2,
-
     subject: "Physics",
-
     chapter: "Units and Measurements",
-
     difficulty: "Easy",
-
     marks: 2,
-
     negativeMarks: 1,
-
-    question:
-      "SI Unit of Force?",
-
+    question: "SI Unit of Force?",
     options: [
       "Newton",
       "Joule",
       "Pascal",
       "Watt"
     ],
-
     answer: 0
   }
 
